9d42e7e86afe37afa4436c0e0b1e0153258e1cec
ena02 <erkembekov.nurzhan@mail.ru>
2022-02-01 14:01:30 +0600

    Implemented Comment

--- src/main/java/com/example/instazoo/entity/Comment.java
+++ src/main/java/com/example/instazoo/entity/Comment.java
@@ -1,4 +1,18 @@
 package com.example.instazoo.entity;
 
+import javax.persistence.PrePersist;
+import java.time.LocalDateTime;
+
 public class Comment {
+    private Long id;
+    private Post post;
+    private String username;
+    private Long userId;
+    private String massage;
+    private LocalDateTime createdDate;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdDate = LocalDateTime.now();
+    }
 }
