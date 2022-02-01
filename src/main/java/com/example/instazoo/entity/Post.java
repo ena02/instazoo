a94552675f1be9d5a845d41874c376874f27e604
ena02 <erkembekov.nurzhan@mail.ru>
2022-02-01 13:56:49 +0600

    Created project

--- src/main/java/com/example/instazoo/entity/Post.java
+++ src/main/java/com/example/instazoo/entity/Post.java
@@ -1,5 +1,26 @@
 package com.example.instazoo.entity;
 
-public class Post {
+import javax.persistence.PrePersist;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+public class Post {
+    private Long id;
+    private String title;
+    private String caption;
+    private String location;
+    private Integer likes;
+
+    private Set<String> likedUsers = new HashSet<>();
+    private User user;
+    private List<Comment> comments = new ArrayList<>();
+    private LocalDateTime createdDate;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdDate = LocalDateTime.now();
+    }
 }
