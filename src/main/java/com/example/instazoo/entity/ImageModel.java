7177de27462a862829bc7a9a486a2d0140b2257b
ena02 <erkembekov.nurzhan@mail.ru>
2022-02-01 13:59:23 +0600

    Implemented ImageModel

--- src/main/java/com/example/instazoo/entity/ImageModel.java
+++ src/main/java/com/example/instazoo/entity/ImageModel.java
@@ -1,4 +1,10 @@
 package com.example.instazoo.entity;
 
 public class ImageModel {
+
+    private Long id;
+    private String name;
+    private byte[] imageBytes;
+    private Long userId;
+    private Long postId;
 }
